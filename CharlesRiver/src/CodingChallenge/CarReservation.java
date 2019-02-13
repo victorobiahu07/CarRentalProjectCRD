faa0fca1dbe372bc22f63e0fc78c65c19a42a34f
victorobiahu07 <vick.obiahu@gmail.com>
2019-02-13 01:00:17 -0500

    Update CarReservation.java

--- CharlesRiver/src/CodingChallenge/CarReservation.java
+++ CharlesRiver/src/CodingChallenge/CarReservation.java
@@ -25,23 +25,7 @@ public class CarReservation {
 		startDate = sDate;
 		endDate = eDate;
 	}
- 
-	/*public String getStartDate() {
-		return startDate;
-	}
 
-	public void setStartDate(String startDate) {
-		this.startDate = startDate;
-	}
-
-	public String getEndDate() {
-		return endDate;
-	}
-	
-	public void setEndDate(String endDate) {
-		this.endDate = endDate;
-	}*/
-	
 	public Date getStartDate() throws ParseException {
 		return sdf.parse(startDate);
 	}
