2d479f9ddce8393c1ec79f7ad174ce4ebc50b632
victorobiahu07 <vick.obiahu@gmail.com>
2019-02-13 10:22:43 -0500

    Update CarRentalClient.java

--- CharlesRiver/src/CodingChallenge/CarRentalClient.java
+++ CharlesRiver/src/CodingChallenge/CarRentalClient.java
@@ -12,24 +12,25 @@ public class CarRentalClient {
 	
 	public static void main(String[] args) throws Exception {
 		
-		CarReservationController reservationSystem = new CarReservationController();
+		CarReservationController reservationController = new CarReservationController();
 		CarReservation firstRes = new CarReservation(VehicleType.PREMIUM, "Jon Snow", "9098070212", "2019-01-23", "2019-01-31");
-		reservationSystem.addReservation(firstRes);
+		reservationController.addReservation(firstRes);
 		
 		CarReservation secondRes = new CarReservation(VehicleType.ECONOMY, "Jamie Lannister", "312999111", "2019-01-11", "2019-01-20");
-		reservationSystem.addReservation(secondRes);
+		reservationController.addReservation(secondRes);
 		
 		CarReservation thirdRes = new CarReservation(VehicleType.PREMIUM, "Jack White", "312999100", "2019-01-15", "2019-01-19");
-		reservationSystem.addReservation(thirdRes);
+		reservationController.addReservation(thirdRes);
 		
 		CarReservation fourthRes = new CarReservation(VehicleType.ECONOMY, "Luke Skywalker", "302919102", "2019-01-10", "2019-01-11");
-		reservationSystem.addReservation(fourthRes);
+		reservationController.addReservation(fourthRes);
 		
 		//testing delete here
-		reservationSystem.deleteReservation(secondRes);
+		reservationController.deleteReservation(secondRes);
 		
-		System.out.println("Number of reservations in reservation map: " + reservationSystem.reservationsMap.size()); //Should return 3 because I added 4 and deleted 1
+		System.out.println("Number of reservations in reservation map: " + reservationController.reservationsMap.size()); //Should return 3 because I added 4 and deleted 1
 	
+		
 		
 		//For extensive test cases, go to CarReservationTest JUnit class 	
 	}
